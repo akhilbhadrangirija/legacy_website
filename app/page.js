f59b38a59c7ee21fbc51ff3bab2023c396a9806c
akhilbhadrangirija <iamakhilbg@gmail.com>
2025-11-22 16:57:34 +0530

    Update home page

--- app/page.js
+++ app/page.js
@@ -23,8 +23,8 @@ export default function Home() {
       <Header />
       <HeroSection />
       <AboutSection />
-      <PhilosophySection />
-      <InternationalStandardsSection />
+      {/* <PhilosophySection />
+      <InternationalStandardsSection /> */}
       {/* <FreedomAndCareSection />
       <FreedomSection />
       <CommunityLifeSection />
